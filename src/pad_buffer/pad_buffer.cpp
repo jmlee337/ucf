9135fe8abc1c7f598f243e73dc0db9d745e2e8c5
jmlee337 <jmlee337@gmail.com>
2024-02-26 17:25:39 +0900

    truncate cardinals to 0.9875

--- src/pad_buffer/pad_buffer.cpp
+++ src/pad_buffer/pad_buffer.cpp
@@ -37,27 +37,14 @@ static bool check_sdrop_up(const auto &buffer, const PlayerInput &input)
 
 [[gnu::noinline]] static void apply_cardinals(const vec2c &raw_stick, vec2 *stick)
 {
-	// Produce 1.0 cardinals when one axis is >= 80 and
-	// the other is within [-SNAP_RANGE, SNAP_RANGE]
-	constexpr auto SNAP_RANGE = 6;
-
 	const auto raw_x = raw_stick.x;
 	const auto raw_y = raw_stick.y;
 
-	if ((raw_x <= -80 || raw_x >= 80) && raw_y >= -SNAP_RANGE && raw_y <= SNAP_RANGE)
-		*(vec2i*)stick = {std::bit_cast<int>(1.f) ^ (std::signbit(raw_stick.x) << 31), 0};
-	else if ((raw_y <= -80 || raw_y >= 80) && raw_x >= -SNAP_RANGE && raw_x <= SNAP_RANGE)
-		*(vec2i*)stick = {0, std::bit_cast<int>(1.f) ^ (std::signbit(raw_stick.y) << 31)};
-}
-
-static bool should_apply_cardinals(const Player *player)
-{
-	switch (player->character_id) {
-	case CID_Zelda:
-		return player->action_state != AS_Zelda_SpecialHiStart;
-	default:
-		return true;
-	}
+	// Truncate 1.0 cardinals to 0.9875
+	if ((raw_x <= -80 || raw_x >= 80) && raw_y == 0)
+		*(vec2i*)stick = {std::bit_cast<int>(0.9875f) ^ (std::signbit(raw_stick.x) << 31), 0};
+	else if ((raw_y <= -80 || raw_y >= 80) && raw_x == 0)
+		*(vec2i*)stick = {0, std::bit_cast<int>(0.9875f) ^ (std::signbit(raw_stick.y) << 31)};
 }
 
 static void gecko_entry()
@@ -70,10 +57,8 @@ static void gecko_entry()
 		buffer->index = (buffer->index + 1) & UCF_PAD_BUFFER_MASK;
 		buffer->entries[buffer->index].stick = status.stick;
 
-		if (should_apply_cardinals(player)) {
-			apply_cardinals(status.stick, &player->input.stick);
-			apply_cardinals(status.cstick, &player->input.cstick);
-		}
+		apply_cardinals(status.stick, &player->input.stick);
+		apply_cardinals(status.cstick, &player->input.cstick);
 
 		if (check_sdrop_up(*buffer, player->input))
 			buffer->sdrop_up_frames++;
